c8c320a547395342f50d9c1a91a152af275ab1aa
evellykarine <eks3@aluno.ifal.edu.br>
2022-09-05 20:22:54 +0000

    adc a classe Divisao

--- api/src/main/java/app/App.java
+++ api/src/main/java/app/App.java
@@ -7,6 +7,7 @@ public class App extends Jooby {
     mvc(new Controller());
     mvc(new Adicao());
     mvc(new Subtracao());
+    mvc(new Divisao());
   }
 
   public static void main(final String[] args) {
