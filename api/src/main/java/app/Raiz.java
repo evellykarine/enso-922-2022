481d31a63d6312207706de2860d5590bc91b82c2
evellykarine <eks3@aluno.ifal.edu.br>
2022-09-05 19:55:54 +0000

    base

---
+++ api/src/main/java/app/Raiz.java
@@ -0,0 +1,12 @@
+package app;
+
+import io.jooby.annotations.*;
+
+@Path("/raiz/{op}")
+public class Raiz {
+
+    @GET
+    public double calculaRaiz(@PathParam double op) {
+        return Math.sqrt(op);
+    }
+}
