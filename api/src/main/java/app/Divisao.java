36f740255d60d351145294574b86f114084b21b6
evellykarine <eks3@aluno.ifal.edu.br>
2022-09-19 13:54:46 +0000

    Correção

---
+++ api/src/main/java/app/Divisao.java
@@ -0,0 +1,12 @@
+package app;
+
+import io.jooby.annotations.*;
+
+@Path("/divisao/{op1}/{op2}")
+public class Divisao{
+
+    @GET
+    public double calculaDivisao (@PathParam double op1, @PathParam double op2) {
+        return op1 / op2;
+    }
+}
