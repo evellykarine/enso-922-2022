08f3be28593e55700928eabb6d0d9a8ce2c00125
Maria Luiza <mlsl5@aluno.ifal.edu.br>
2022-09-26 19:49:29 +0000

    adicao_test

---
+++ api/src/test/java/app/AdicaoTest.java
@@ -0,0 +1,35 @@
+package app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AdicaoTest {
+    
+    Adicao adicao;
+
+    @BeforeEach
+    public void beforeEach() {
+        adicao = new Adicao();
+    }
+
+
+    @Test
+    public void testCasoBase() {
+        double resultado = adicao.calculaAdicao(2,7);
+        assertEquals(9, resultado);
+    }
+
+    @Test
+    public void testAdicaodeNegativos() {
+        double resultado = adicao.calculaAdicao(-7,-2);
+        assertEquals(-9, resultado);
+    }
+
+    @Test
+    public void testAdicaodeNegativoePosi() {
+        double resultado = adicao.calculaAdicao(-7,2);
+        assertEquals(-5, resultado);
+    }
+}
